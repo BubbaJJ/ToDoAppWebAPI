fc688882b0865042dac437b246dda23930c2caa4
Jim Kaya <Jim.kaya@gmail.com>
2022-05-19 14:41:43 +0200

    Add project files.

---
+++ ToDoAppWebAPI/Controllers/TodoController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoAppWebAPI.Database;
+using ToDoAppWebAPI.Models;
+
+namespace ToDoAppWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Todo/GetTodos
+        [HttpGet("GetTodos")] // Unik URL
+        public async Task<ActionResult<IEnumerable<Todo>>> GetAllTodos()
+        {
+            return await _context.Todos.ToListAsync();
+        }
+
+        // GET api/Todo/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Todo>> GetTodoById(int id)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return todo;
+        }
+
+        // POST api/Todo
+        [HttpPost]
+        public async Task<ActionResult<Todo>> CreateTodo(Todo todo)
+        {
+            _context.Todos.Add(todo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodoById", new { id = todo.Id }, todo);
+        }
+
+        // PUT api/<TodoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/Todo/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Todo>> DeleteTodo(int id)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Todos.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ ToDoAppWebAPI/Models/Todo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoAppWebAPI.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Completed { get; set; } = false;
+        public DateTime DateAdded { get; set; } = DateTime.Now;
+        public DateTime? DateFinished { get; set; }
+        public DateTime DueDate { get; set; } = DateTime.Now.AddDays(3);
+        public User AssignedTo { get; set; }
+    }
+}
